e08c8706979087dc9cbd7d333cbf7087a6b65a75
YinanZhao <64669960+YinanZhao@users.noreply.github.com>
2022-01-08 04:26:44 -0500

    One last time .. same bug

--- src/controller/PicturesController.js
+++ src/controller/PicturesController.js
@@ -4,7 +4,7 @@ const NASA_URL = `https://api.nasa.gov/planetary/apod/`;
 
 export const getPicture = async () => {
   return new Promise((resolve) => {
-    fetch(`${NASA_URL}?api_key=${API_KEY}`.replace('http:', 'https'), {
+    fetch(`${NASA_URL}?api_key=${API_KEY}`.replace('http:', 'https:'), {
       method: "GET",
     }).then((res) => {
       resolve(res.json());
@@ -17,7 +17,7 @@ export const getPicturesByDate = async (startDate, endDate) => {
 
   // dates need to be parsed into correct format
   return new Promise((resolve) => {
-    fetch(`${NASA_URL}?api_key=${API_KEY}&start_date=${new Date(startDate).toISOString().slice(0, 10)}&end_date=${new Date(endDate).toISOString().slice(0, 10)}`.replace('http', 'https'), {
+    fetch(`${NASA_URL}?api_key=${API_KEY}&start_date=${new Date(startDate).toISOString().slice(0, 10)}&end_date=${new Date(endDate).toISOString().slice(0, 10)}`.replace('http:', 'https:'), {
       method: "GET",
     }).then((res) => {
       resolve(res.json());
